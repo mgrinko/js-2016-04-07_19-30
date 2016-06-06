879a0c4b0eb64810b07af1c944a7637c964730a7
mgrinko <glukym@gmail.com>
2016-06-06 20:59:36 +0300

    Phones catalogue is now rendered

--- scripts/catalogue.js
+++ scripts/catalogue.js
@@ -1,12 +1,31 @@
 'use strict';
 
+const catalogueTemplate = `
+  <ul class="phones">
+
+    <% phones.forEach(function(phone) {%>
+
+      <li class="thumbnail phone-listing">
+        <a href="#/phones/<%= phone.id %>" class="thumb">
+          <img src="<%= phone.imageUrl %>" alt="<%- phone.name %>">
+        </a>
+        <a href="#/phones/<%= phone.id %>"><%= phone.name %></a>
+        <p><%- phone.snippet %></p>
+      </li>
+
+    <% }); %>
+  </ul>
+`;
+
 class Catalogue {
   constructor(options) {
     this._el = options.element;
 
   }
 
-  render(phones) {
-    this._el.innerHTML = 'Phone catalogue'
+  render(arrayOfPhones) {
+    this._el.innerHTML = _.template(catalogueTemplate)({
+      phones: arrayOfPhones
+    });
   }
 }
